bf6b0cdfb97b551cad2d4d68ab7534b57c2cd757
draiguv <dcrauv13@gmail.com>
2023-07-22 23:45:32 +0800

    Create components and simplify code dynamically

--- src/data/emojipedia.js
+++ src/data/emojipedia.js
@@ -19,5 +19,35 @@ const emojipedia = [
     name: "Rolling On The Floor, Laughing",
     meaning:
       "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
-  }
+  },
+  {
+    id: 4,
+    emoji: "😡",
+    name: "Pouting Face",
+    meaning:
+      "The pouting face expresses displeasure with a person or situation. Beware, danger of explosion! Furious emoji face. Is annoyed and already sulking with a deep-red face."
+  },
+  {
+    id: 5,
+    emoji: "😎",
+    name: "Face With Sunglasses",
+    meaning:
+      "I'm the king of the world! Smart and casual, this 😎 emoji features a face with a wide, closed smile and black sunglasses. It expresses coolness and confidence and is used to convey a carefree, relaxed attitude or to express that something is excellent. If you are confident in yourself, have done something particularly well and are proud of it, this emoji is an appropriate choice. It is also a symbol of sunshine, summer vacations, and beach vacations. It can be used in situations where you feel a sense of superiority or serenity. In some cases, the emoji even conveys a bit of irony or fun in your own coolness. All in all, it's versatile and helps convey positive emotions and attitudes in communication."
+  },
+  {
+    id: 6,
+    emoji: "👻",
+    name: "Ghost",
+    meaning:
+      "The funny-smiling ghost is too cute to frighten others or cause harm. Symbol for Halloween or jokingly for creepy things, e.g. movies."
+  },
+  {
+    id: 7,
+    emoji: "👽",
+    name: "Alien",
+    meaning:
+      "The aliens are here! Symbol for an extraterrestrial being. However, smiles friendly and comes in peace."
+  },
 ];
+
+export default emojipedia
